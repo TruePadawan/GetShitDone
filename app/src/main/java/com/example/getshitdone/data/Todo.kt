30e7226190bc4077a631c1c4353684492832eb9e
Hermes <hermeschigoziri@gmail.com>
2025-03-12 15:32:34 +0100

    define the data layer

--- app/src/main/java/com/example/getshitdone/data/Todo.kt
+++ app/src/main/java/com/example/getshitdone/data/Todo.kt
@@ -1,3 +1,30 @@
 package com.example.getshitdone.data
 
-data class Todo(var title: String, var desc: String? = "", var isComplete: Boolean = false)
+import java.util.UUID
+
+data class Todo(
+    val id: String = UUID.randomUUID().toString(),
+    var title: String, var desc: String? = "", var isComplete: Boolean = false
+)
+
+class LocalTodosDataSource(private val todos: Map<String, Todo> = mapOf<String, Todo>()) {
+    fun fetchTodos() = todos
+}
+
+class TodoRepository() {
+    private val localTodosDataSource = LocalTodosDataSource()
+
+    fun getAllTodos(): Map<String, Todo> {
+        return localTodosDataSource.fetchTodos()
+    }
+
+    fun getTodoById(id: String): Todo? {
+        val todos = getAllTodos()
+        return todos[id]
+    }
+
+    fun getTodosByCompletionStatus(isComplete: Boolean): Map<String, Todo> {
+        val todos = getAllTodos()
+        return todos.filter { it.value.isComplete == isComplete }
+    }
+}
