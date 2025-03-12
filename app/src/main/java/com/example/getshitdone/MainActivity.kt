37ee6a3de3ec412442309af15b4fd8a7aa63e506
Hermes <hermeschigoziri@gmail.com>
2025-03-12 15:36:34 +0100

    Optimize imports and remove unused code

--- app/src/main/java/com/example/getshitdone/MainActivity.kt
+++ app/src/main/java/com/example/getshitdone/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,9 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,7 +45,6 @@ class MainActivity : ComponentActivity() {
 */
 @Composable
 fun GetShitDoneApp(modifier: Modifier = Modifier) {
-//    val createTodoDialogIsOpen by remember { mutableStateOf(false) }
     val todos = remember { mutableStateListOf<Todo>() }
 
     Column(modifier = modifier.padding(8.dp)) {
